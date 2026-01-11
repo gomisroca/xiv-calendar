b9a6f757c6b7add210f8df781f61f04289da5670
Adrià Gomis <gomisroca@gmail.com>
2026-01-11 13:46:37 +0100

    simple discord bot to confirm implementation

---
+++ bot/index.js
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import { Client, GatewayIntentBits } from "discord.js";
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+});
+
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user?.tag}`);
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
