17eb4dc7ba5ada65d8ab6f43cdd2c923dfc1d2af
Adrià Gomis <gomisroca@gmail.com>
2026-01-12 12:13:24 +0100

    update parent on rsvp

--- src/app/orgs/[slug]/[eventId]/rsvp-buttons.tsx
+++ src/app/orgs/[slug]/[eventId]/rsvp-buttons.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useTransition } from "react";
 import { rsvpToEvent } from "@/server/actions/events";
 import { EventStatus } from "generated/prisma";
+import { useRouter } from "next/navigation";
 
 interface RSVPButtonsProps {
   eventId: string;
@@ -15,15 +16,14 @@ export default function RSVPButtons({
 }: RSVPButtonsProps) {
   const [status, setStatus] = useState<EventStatus | undefined>(initialStatus);
   const [loading, setLoading] = useState(false);
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
   const [error, setError] = useState<string | null>(null);
 
   const handleRSVP = async (newStatus: EventStatus) => {
     if (loading) return;
     setLoading(true);
-    setError(null);
-
-    // Optimistic update
-    const prevStatus = status;
     setStatus(newStatus);
 
     try {
@@ -31,8 +31,12 @@ export default function RSVPButtons({
       if (!result.success) {
         throw new Error(result.error || "RSVP failed");
       }
+
+      startTransition(() => {
+        router.refresh();
+      });
     } catch (err: unknown) {
-      setStatus(prevStatus); // revert
+      setStatus(initialStatus); // revert
       setError(err instanceof Error ? err.message : "Unknown error");
     } finally {
       setLoading(false);
@@ -47,7 +51,7 @@ export default function RSVPButtons({
             ? "bg-green-500 text-white"
             : "bg-gray-200"
         }`}
-        disabled={loading}
+        disabled={loading || isPending}
         onClick={() => handleRSVP(EventStatus.ATTENDING)}
       >
         ✅ Attend
@@ -59,7 +63,7 @@ export default function RSVPButtons({
             ? "bg-red-500 text-white"
             : "bg-gray-200"
         }`}
-        disabled={loading}
+        disabled={loading || isPending}
         onClick={() => handleRSVP(EventStatus.NOT_ATTENDING)}
       >
         ❌ Not Attend
